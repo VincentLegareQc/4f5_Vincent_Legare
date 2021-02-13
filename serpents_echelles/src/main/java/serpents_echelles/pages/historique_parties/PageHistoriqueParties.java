c7ad901c67de289706c59ce0db2f28f5ae34fd3c
Vincent Legare <vincent.legare.qc@hotmail.com>
2021-02-13 13:51:52 -0500

    module 2

---
+++ serpents_echelles/src/main/java/serpents_echelles/pages/historique_parties/PageHistoriqueParties.java
@@ -0,0 +1,9 @@
+package serpents_echelles.pages.historique_parties;
+
+public class PageHistoriqueParties {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Bonjour!");
+	}
+}
